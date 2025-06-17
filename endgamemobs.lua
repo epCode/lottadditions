d6277b75bc5afac1d0e48369d9cbd0632c84900e
epCode <eeliperry@gmail.com>
2025-06-17 15:58:35 -0600

    superger

--- endgamemobs.lua
+++ endgamemobs.lua
@@ -90,3 +90,80 @@ mobs:register_mob("lottadditions:gollum", {
 	
 })
 mobs:register_spawn("lottadditions:gollum", {"default:stone"}, 20, -1, 100000, 2, -7000)
+
+mobs:register_mob("lottadditions:ogre", {
+	type = "npc",
+    race = "GAMEorc",
+	
+	hp_min = 220,
+	hp_max = 230,
+	
+	collisionbox = {-1, -2.1, -1, 1, 2.3, 1},
+	visual = "mesh",
+	mesh = "ogre.b3d",
+	textures = {
+		{"ad_ogre.png"},
+	},
+	visual_size = {x = 5.5, y = 5.5},
+	rotate = 180,
+	do_custom = function(self, dtime, moveresult)
+		if self.attack then
+			local cols = moveresult.collisions
+			for _,def in pairs(cols) do
+				if def.axis ~= "y" and def.node_pos and math.random(1) == 1 then
+					local node = minetest.get_node(def.node_pos)
+					local drops = core.get_node_drops(node)
+					for _,stack in pairs(drops) do
+						core.add_item(def.node_pos, stack)
+					end
+					core.dig_node(def.node_pos)
+				end
+			end
+		end
+	end,
+	makes_footstep_sound = true,
+	view_range = 15,
+	walk_velocity = 1,
+	run_velocity = 4,
+	damage = 22,
+	reach = 8,
+	armor = 100,
+	light_resistant = true,
+	drawtype = "front",
+	water_damage = 0,
+	lava_damage = 10,
+	light_damage = 0,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	
+	drops = {
+      {name = "dmobs:dragon_gem_poison", chance = 100, min = 1, max = 1},
+	  {name = "dmobs:dragon_gem_lightning", chance = 100, min = 1, max = 1},
+	  {name = "dmobs:dragon_gem_fire", chance = 100, min = 1, max = 1},
+	  {name = "dmobs:dragon_gem_ice", chance = 100, min = 1, max = 1},
+   },
+	
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 79,
+		walk_start = 168,
+		walk_end = 187,
+		run_start = 168,
+		run_end = 187,
+		punch_start = 189,
+		punch_end = 198,
+	},
+	jump = true,
+	sounds = {
+		war_cry = {"ogre_war"},
+		death = "ogre_death",
+		attack = {"ogre_attack"},
+	},
+	attacks_monsters = true,
+	peaceful = true,
+	group_attack = true,
+	step = 1,
+})
+mobs:spawn_specific("lottadditions:ogre", {"default:stone"}, {"air"}, -1, 10, 60, 10000, 3, -30000, -6000)
