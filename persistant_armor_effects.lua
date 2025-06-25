e58abc682917bd824cdc4979a34cc823e1d4d4a8
epCode <eeliperry@gmail.com>
2025-06-25 11:56:22 -0600

    Fix hotbar moving stuff

--- persistant_armor_effects.lua
+++ persistant_armor_effects.lua
@@ -1,7 +1,9 @@
-
+local pameffecrs = {}
 
 core.register_globalstep(function(dtime)
   for _,player in pairs(minetest.get_connected_players()) do
+    local name = player:get_player_name()
+    pameffecrs[name] = pameffecrs[name] or {}
     lottadditions.persistant_armor[player] = lottadditions.persistant_armor[player] or {
       [1] = "",
       [2] = "",
@@ -13,10 +15,45 @@ core.register_globalstep(function(dtime)
     }
     local old_armor = lottadditions.persistant_armor[player] or ""
     local inv = player:get_inventory()
+    local armor_inv = minetest.get_inventory({type="detached", name=name.."_armor"})
     local new_armor = {}
-    for i = 1, 11 do
+    for i = 1, 10 do
 
       local stack = inv:get_stack("armor", i)
+
+
+      if i > 5 then
+        if pameffecrs[name].oldinv then
+          local pinv = pameffecrs[name].oldinv
+          local parmorinv = pameffecrs[name].oldarmorinv
+          local relstack = parmorinv[i]
+          --print(inv:get_stack("main", i-5):get_name())
+          local stop
+          if pinv[i-5]:get_name() ~= inv:get_stack("main", i-5):get_name() then
+            relstack = inv:get_stack("main", i-5)
+          elseif armor_inv:get_stack("armor", i):get_name() ~= parmorinv[i]:get_name() then
+            relstack = armor_inv:get_stack("armor", i)
+          else
+            stop = true
+          end
+          if not stop then
+            if relstack:get_name() == "" then
+              relstack = ItemStack("lottarmor:placeholder")
+            end
+            local armstack = relstack
+            if relstack:get_name() == "lottarmor:placeholder" then
+              armstack = ItemStack("")
+            end
+            
+            pameffecrs[name].oldinv[i-5] = relstack
+            pameffecrs[name].oldarmorinv[i] = armstack
+            inv:set_stack("main", i-5, relstack)
+            inv:set_stack("armor", i, armstack)
+            armor_inv:set_stack("armor", i, armstack)
+          end
+        end
+      end
+
 
       local item = stack:get_name()
 
@@ -32,11 +69,16 @@ core.register_globalstep(function(dtime)
       end
       if old_armor[i] ~= "" and old_armor[i] ~= new_armor[i] and minetest.registered_items[old_armor[i]] and minetest.registered_items[old_armor[i]].removal then
         minetest.registered_items[old_armor[i]].removal(player, stack)
-        print("s")
       end
       if new_armor[i] ~= "" and old_armor[i] ~= new_armor[i] and minetest.registered_items[new_armor[i]] and minetest.registered_items[new_armor[i]].equip then
         minetest.registered_items[new_armor[i]].equip(player, stack)
       end
+    end
+    pameffecrs[name].oldinv = {}
+    pameffecrs[name].oldarmorinv = {}
+    for i = 1, 10 do
+      pameffecrs[name].oldinv[i] = inv:get_stack("main", i)
+      pameffecrs[name].oldarmorinv[i] = armor_inv:get_stack("armor", i)
     end
     lottadditions.persistant_armor[player] = new_armor
   end
